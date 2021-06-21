5627e69fbb51dee82f24dfebfff00d09b4677038
Cheney <czh419779202@163.com>
2021-06-21 17:36:46 +0800

    add grontine func

--- bootstrap/base.go
+++ bootstrap/base.go
@@ -1,4 +1,24 @@
 package bootstrap
 
-type Boot struct {
+import (
+	"context"
+)
+
+/**
+*app执行入口
+ */
+type AppServer struct {
+	Ctx    context.Context
+	Cancel context.CancelFunc
+}
+
+/**
+*注册执行函数
+ */
+func (app *AppServer) Setup() {
+
+}
+
+func (app *AppServer) Start() {
+
 }
--- library/pool/pool.go
+++ library/pool/pool.go
@@ -1,58 +1,82 @@
 package pool
 
-import "fmt"
+import (
+	"sync"
+
+	res "github.com/easydb/library/res"
+)
+
+const (
+	defaultRuntineNumber = 10
+	defailtTotal         = 10
+)
 
 type Pool struct {
-	Queue         chan func() error
-	RuntineNumber int
-	Total         int
-
-	Result         chan error
+	mutex          sync.WaitGroup
+	Queue          chan *res.Request
+	RuntineNumber  int
+	Total          int
+	result         chan map[string]*res.Result
+	taskResponse   []*res.Reponse
 	FinishCallback func()
 }
 
 func (this *Pool) Init(runtineNumber, total int) {
 	this.RuntineNumber = runtineNumber
 	this.Total = total
-	this.Queue = make(chan func() error, total)
-	this.Result = make(chan error, total)
+	this.Queue = make(chan *res.Request, runtineNumber)
+	this.result = make(chan map[string]*res.Result, runtineNumber)
 }
 
 func (this *Pool) Start() {
-	for i := 0; i < this.RuntineNumber; i++ {
-		go func() {
-			for {
-				task, ok := <-this.Queue
-				if !ok {
-					break
-				}
-				err := task()
-				this.Result <- err
-			}
-		}()
+	if this.RuntineNumber <= 0 {
+		return
 	}
-
-	for j := 0; j < this.Total; j++ {
-		res, ok := <-this.Result
+	for i := 0; i < this.RuntineNumber; i++ {
+		this.mutex.Add(1)
+		go func(num int) {
+			defer this.mutex.Done()
+			task, ok := <-this.Queue
+			if !ok {
+				return
+			}
+			taskName := task.Name
+			taskResult := task.Func()
+			result := map[string]*res.Result{
+				taskName: taskResult,
+			}
+			this.result <- result
+		}(i)
+	}
+	this.mutex.Wait()
+	this.taskResponse = []*res.Reponse{}
+	for j := 0; j < this.RuntineNumber; j++ {
+		result, ok := <-this.result
 		if !ok {
 			break
 		}
-		if res != nil {
-			fmt.Println(res)
+		response := res.ReponseIntance()
+		for key, value := range result {
+			response.Name = key
+			response.Value = value
+			this.taskResponse = append(this.taskResponse, response)
 		}
 	}
-
 	if this.FinishCallback != nil {
 		this.FinishCallback()
 	}
 }
 
-func (this *Pool) Stop() {
-	close(this.Queue)
-	close(this.Result)
+func (this *Pool) TaskResult() []*res.Reponse {
+	return this.taskResponse
 }
 
-func (this *Pool) AddTask(task func() error) {
+func (this *Pool) Stop() {
+	close(this.Queue)
+	close(this.result)
+}
+
+func (this *Pool) AddTask(task *res.Request) {
 	this.Queue <- task
 }
 
