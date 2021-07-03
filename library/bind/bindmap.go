0ff3a7c4269c761a702729be06301d75cdaccb34
Cheney <czh419779202@163.com>
2021-07-03 17:45:07 +0800

    mergemain

---
+++ library/bind/bindmap.go
@@ -0,0 +1,11 @@
+package bind
+
+import (
+	"fmt"
+
+	"github.com/rainmyy/easyDB/library/strategy"
+)
+
+func BindMap(tree *strategy.TreeStruct, obj map[interface{}]interface{}) {
+	fmt.Print(1111)
+}
