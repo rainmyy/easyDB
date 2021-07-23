9c9e23bb4fbed0ea1c0ba9b852996b757fe60e6b
Cheney <czh419779202@163.com>
2021-07-23 17:27:45 +0800

    add yaml

--- library/file/parser_yaml.go
+++ library/file/parser_yaml.go
@@ -8,16 +8,10 @@ import (
 )
 
 func ParserYamlContent(data []byte) ([]*TreeStruct, error) {
-	yamlContent := `
-field1: 小韩说课
-field2:
-  field3: value
-  field4: [42, 1024]
-`
 	// 存储解析数据
 	result := make(map[string]interface{})
 	// 执行解析
-	err := yaml.Unmarshal([]byte(yamlContent), &result)
+	err := yaml.Unmarshal([]byte(data), &result)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
